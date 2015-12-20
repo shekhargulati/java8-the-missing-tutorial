774affedda5822fc3d6c5564daeea3d22536c81c
Shekhar Gulati <shekhargulati84@gmail.com>
2015-12-20 23:46:52 +0530

    updated ch01

--- code/src/main/java/com/shekhargulati/java8_tutorial/ch06/MapExample.java
+++ code/src/main/java/com/shekhargulati/java8_tutorial/ch06/MapExample.java
@@ -1,7 +1,19 @@
 package com.shekhargulati.java8_tutorial.ch06;
 
-/**
- * Created by shekhargulati on 20/12/15.
- */
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+
 public class MapExample {
+
+    public static <T, U> Map<T, U> createMap(SimpleEntry<T, U>... entries) {
+        return Stream.of(entries).collect(toMap(SimpleEntry::getKey, SimpleEntry::getValue));
+    }
+
+    public static <T, U> void doSth(Map<T, U> map) {
+
+
+    }
 }
--- code/src/test/java/com/shekhargulati/java8_tutorial/ch06/MapExampleTest.java
+++ code/src/test/java/com/shekhargulati/java8_tutorial/ch06/MapExampleTest.java
@@ -1,10 +1,19 @@
 package com.shekhargulati.java8_tutorial.ch06;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
-/**
- * Created by shekhargulati on 20/12/15.
- */
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 public class MapExampleTest {
 
+    @Test
+    public void shouldCreateAHashMapUsingSimpleEntries() throws Exception {
+        Map<String, Integer> nameAndAge = MapExample.createMap(new SimpleEntry<>("shekhar", 32), new SimpleEntry<>("rahul", 33), new SimpleEntry<>("sameer", 33));
+        assertThat(nameAndAge.size(), equalTo(3));
+        
+    }
 }
